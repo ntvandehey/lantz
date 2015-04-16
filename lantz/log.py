ed8340099e9cf7fd18d13714164508169450fed4
ntvandehey <nickvandehey@gmail.com>
2015-04-16 16:25:32 -0700

    update logy.py to fix colorama bug
    lines 19-20 added. 
    solves problem

--- lantz/log.py
+++ lantz/log.py
@@ -16,6 +16,8 @@ import socket
 import struct
 import logging
 import threading
+import colorama
+from colorama import Fore, Back, Style
 
 from logging import DEBUG, INFO, WARNING, ERROR, CRITICAL
 from logging.handlers import SocketHandler, DEFAULT_TCP_LOGGING_PORT, DEFAULT_UDP_LOGGING_PORT
